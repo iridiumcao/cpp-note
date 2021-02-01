57faaf67628c127e9750269962c2fe288ae0afed
Cao Yi <yi.cao@etix.com>
2021-02-01 17:14:36 +0800

    temp

--- code/ch01/ch01.1.cpp
+++ code/ch01/ch01.1.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-int sum(int a, int b) {
+int sum(int a, int b)
+{
     return a + b;
 }
 
-int main() {
+int main()
+{
     int (*pf)(int, int); // 定义函数指针
-    pf = sum; // pf 指向 sum 函数
+    pf = sum;            // pf 指向 sum 函数
     cout << pf(3, 2) << endl;
     return 0;
 }
